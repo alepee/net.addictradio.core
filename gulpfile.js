60540c320128dcafbbf60a0abee77ee336ef0470
Antoine Lépée <alepee@gmail.com>
2015-11-18 14:08:18 +0100

    remove useless return

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ gulp.task('connect', function() {
 });
 
 gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
+  bower.commands.install()
     .on('log', function(data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
