16652dfcaa8c96542bfd711cc967489973e821ac
Antoine Lépée <alepee@gmail.com>
2015-11-18 12:55:20 +0100

    add auto-install via gulp and live reload for demo app

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var concat = require('gulp-concat');
 var annotate = require('gulp-ng-annotate');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var connect = require('gulp-connect');
+var bower = require('bower');
+var sh = require('shelljs');
 
 gulp.task('default', ['compile', 'connect', 'watch']);
 
@@ -14,7 +17,8 @@ gulp.task('compile', function() {
   .pipe(gulp.dest('dist'))
   .pipe(uglify())
   .pipe(rename('core.min.js'))
-  .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('dist'))
+  .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
@@ -26,4 +30,24 @@ gulp.task('connect', function() {
     root: 'demo',
     livereload: true
   });
+});
+
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
 });
